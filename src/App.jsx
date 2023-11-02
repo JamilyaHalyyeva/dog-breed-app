6ec2686e1a93d287925d8056a7305b7c322931a0
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2023-11-02 01:29:22 +0100

    react router  is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import MasterLayout from "./layouts/MasterLayout";
+import { Home } from "./pages/Home";
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MasterLayout />}>
+            <Route path="/" element={<Home/>} ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
